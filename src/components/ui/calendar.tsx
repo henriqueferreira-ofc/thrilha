9859f254fdde890e4fc3c53bf1b339c0adc15a68
Henrique Araújo <henriqueanalista.ads@gmail.com>
2025-05-21 15:21:40 -0300

    update calendar

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,10 +54,7 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        iconLeft: (props) => <ChevronLeft className="h-4 w-4" {...props} />,
-        iconRight: (props) => <ChevronRight className="h-4 w-4" {...props} />
-      }}
+      
       {...props}
     />
   )
